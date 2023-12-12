b3a222fa9890b428f0736a657f110269bba0965d
chelfonseca <michelbeserradafonseca@gmail.com>
2023-12-12 17:40:45 -0300

    skip to next project - study about p5

---
+++ 04_sketch/sketch.js
@@ -0,0 +1,13 @@
+console.log("sketch init");
+
+function setup() {
+    console.log("sketch setup");
+    let c = createCanvas(400, 400);
+    c.position(0,0);
+    clear();
+}
+
+function draw(){
+    background(0);
+    console.log("sketch draw");
+}
